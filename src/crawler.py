952f4715eaceafe95eb9256505d3e586ba46b84b
Archie Simpson <ajm.simpson@outlook.com>
2026-04-04 14:48:00 +0100

    feat(crawler): _is_same_domain via urlparse netloc, drops the stray __init__

--- src/crawler.py
+++ src/crawler.py
@@ -1,5 +1,7 @@
 """Web crawler for the COMP3011 search engine."""
 
+from urllib.parse import urlparse
+
 import requests
 
 
@@ -14,3 +16,7 @@ class Crawler:
         )
         self.pages = {}
         self.visited = set()
+
+    def _is_same_domain(self, url):
+        """Return True when url is on the same netloc as the seed."""
+        return urlparse(url).netloc == urlparse(self.base_url).netloc

0b1e0e17cc9f7df84e5f6d1f7009862a6b47e81e
Archie Simpson <ajm.simpson@outlook.com>
2026-04-26 10:00:00 +0100

    test(search): phrase bonus, print_index output, suggest prefix coverage

--- tests/test_search.py
+++ tests/test_search.py
@@ -90,3 +90,142 @@ class TestSearchFind:
     def test_find_scores_are_positive(self):
         results = self.engine.find("good")
         assert all(r[1] > 0 for r in results)
+
+
+class TestSearchPhraseBonus:
+
+    def test_adjacent_words_rank_higher(self):
+        engine, _ = make_engine(
+            ("http://phrase.com",
+             "<p>good friends together always</p>"),
+            ("http://separate.com",
+             "<p>good things and friends everywhere</p>"),
+        )
+        results = engine.find("good friends")
+        assert results[0][0] == "http://phrase.com"
+
+    def test_phrase_bonus_not_applied_to_single_term(self):
+        engine, _ = make_engine(
+            ("http://a.com", "<p>good good good</p>"),
+        )
+        results = engine.find("good")
+        assert len(results) == 1
+
+    def test_phrase_bonus_zero_when_not_adjacent(self):
+        engine, _ = make_engine(
+            ("http://a.com", "<p>good day bad friends</p>"),
+        )
+        bonus = engine._phrase_bonus(['good', 'friends'], 'http://a.com')
+        assert bonus == 0.0
+
+    def test_phrase_bonus_positive_when_adjacent(self):
+        engine, _ = make_engine(
+            ("http://a.com", "<p>good friends</p>"),
+        )
+        bonus = engine._phrase_bonus(['good', 'friends'], 'http://a.com')
+        assert bonus > 0.0
+
+    def test_phrase_bonus_returns_zero_for_missing_url(self):
+        engine, _ = make_engine(
+            ("http://a.com", "<p>good friends</p>"),
+        )
+        bonus = engine._phrase_bonus(
+            ['good', 'friends'], 'http://notindexed.com')
+        assert bonus == 0.0
+
+    def test_phrase_bonus_scales_with_occurrences(self):
+        engine, _ = make_engine(
+            ("http://a.com",
+             "<p>good friends good friends good friends</p>"),
+        )
+        bonus = engine._phrase_bonus(['good', 'friends'], 'http://a.com')
+        assert bonus >= 6.0
+
+    def test_three_word_phrase_detected(self):
+        engine, _ = make_engine(
+            ("http://a.com", "<p>the good life</p>"),
+            ("http://b.com", "<p>the life good</p>"),
+        )
+        results = engine.find("the good life")
+        assert results[0][0] == "http://a.com"
+
+
+class TestSearchPrintIndex:
+
+    def setup_method(self):
+        self.engine, _ = make_engine(
+            ("http://a.com", "<p>hello world hello</p>"),
+        )
+
+    def test_word_not_found_message(self, capsys):
+        self.engine.print_index("notaword")
+        captured = capsys.readouterr()
+        assert "not found" in captured.out.lower()
+
+    def test_empty_word_message(self, capsys):
+        self.engine.print_index("")
+        captured = capsys.readouterr()
+        assert len(captured.out) > 0
+
+    def test_valid_word_prints_url(self, capsys):
+        self.engine.print_index("hello")
+        captured = capsys.readouterr()
+        assert "http://a.com" in captured.out
+
+    def test_valid_word_prints_count(self, capsys):
+        self.engine.print_index("hello")
+        captured = capsys.readouterr()
+        assert "2" in captured.out
+
+    def test_valid_word_prints_tfidf_header(self, capsys):
+        self.engine.print_index("hello")
+        captured = capsys.readouterr()
+        assert "TF-IDF" in captured.out
+
+    def test_valid_word_prints_positions(self, capsys):
+        self.engine.print_index("hello")
+        captured = capsys.readouterr()
+        assert "[" in captured.out
+
+    def test_word_case_insensitive(self, capsys):
+        self.engine.print_index("HELLO")
+        captured = capsys.readouterr()
+        assert "http://a.com" in captured.out
+
+
+class TestSearchSuggest:
+
+    def setup_method(self):
+        self.engine, _ = make_engine(
+            ("http://a.com",
+             "<p>good goodbye goodness great green</p>"),
+        )
+
+    def test_suggest_returns_completions(self):
+        suggestions = self.engine.suggest("go")
+        assert "good" in suggestions
+
+    def test_suggest_no_match_returns_empty(self):
+        assert self.engine.suggest("zzz") == []
+
+    def test_suggest_empty_string_returns_empty(self):
+        assert self.engine.suggest("") == []
+
+    def test_suggest_max_five_results(self):
+        suggestions = self.engine.suggest("go")
+        assert len(suggestions) <= 5
+
+    def test_suggest_case_insensitive(self):
+        suggestions = self.engine.suggest("GO")
+        assert "good" in suggestions
+
+    def test_suggest_exact_word_included(self):
+        suggestions = self.engine.suggest("good")
+        assert "good" in suggestions
+
+    def test_suggest_returns_list(self):
+        suggestions = self.engine.suggest("go")
+        assert isinstance(suggestions, list)
+
+    def test_suggest_none_input_returns_empty(self):
+        assert self.engine.suggest(None) == []
